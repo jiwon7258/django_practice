7bd5892cf0a38eff7d1722c008296a6032acbacd
jiwon7258 <jiwon7258@gmail.com>
2020-11-14 11:26:54 +0900

    .

--- example.py
+++
@@ -1,7 +0,0 @@
-""" 
-웹 클라리언트
-웹 서버에 HTTP 요청을 보내는 웹 클라이언트
- """
-import urllib.request
-
-print(urllib.request.urlopen("http://www.example.com").read().decode("utf-8"))
--- model.py
+++
@@ -1,8 +0,0 @@
-# models 모듈을 import
-from django.db import models
-from . import views
-
-# models 모듈의 Model 클래스를 상속
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
